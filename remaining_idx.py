bb5479287d13df7551b4063082bd003538462ea4
Pi <s_molling@hotmail.com>
2024-07-23 21:44:47 +0200

    Clean up

--- remaining_idx.py
+++ remaining_idx.py
@@ -5,7 +5,7 @@ import numpy as np
 This file is used to construct remaining test cases for LCR-rot-hop++ for each augmented data set.
 """
 # Parse the content into a list of floats
-remaining_pos_vector = np.load(f"remaining_test_indices_{FLAGS.year}")
+remaining_pos_vector = np.load(f"remaining_test_indices_{FLAGS.year}.npy")
 outF= open(FLAGS.remaining_test_path, "w")
 
 print(FLAGS.test_path)
